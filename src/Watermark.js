0ed97af12d0e5bc784e605d8f305d438d6cfb2aa
JerryShen <jerry82422@gmail.com>
2019-10-23 08:10:24 +0800

    Re-structure `Watermark`

--- src/Watermark.js
+++ src/Watermark.js
@@ -11,6 +11,9 @@ import DateFnsUtils from '@date-io/date-fns';
 
 const size = 1080;
 // const logoRatio = 0.2;
+const hPadding = 80;
+const vPadding = 30;
+const fontSize = 50;
 
 const imageUpload = (e, { setRaw }) => {
   e.preventDefault();
@@ -25,6 +28,78 @@ const imageUpload = (e, { setRaw }) => {
   reader.readAsDataURL(file);
 };
 
+const drawFrame = ({ ctx, size }) => {
+  ctx.lineWidth = 5;
+  ctx.strokeStyle = '#fafafa';
+
+  ctx.beginPath();
+  ctx.moveTo(hPadding, vPadding);
+  ctx.lineTo(size - hPadding, vPadding);
+  ctx.stroke();
+  ctx.closePath();
+
+  ctx.beginPath();
+  ctx.moveTo(size - vPadding, hPadding);
+  ctx.lineTo(size - vPadding, size - hPadding);
+  ctx.stroke();
+  ctx.closePath();
+
+  ctx.beginPath();
+  ctx.moveTo(size - hPadding, size - vPadding);
+  ctx.lineTo(hPadding, size - vPadding);
+  ctx.stroke();
+  ctx.closePath();
+
+  ctx.beginPath();
+  ctx.moveTo(vPadding, size - hPadding);
+  ctx.lineTo(vPadding, hPadding);
+  ctx.stroke();
+  ctx.closePath();
+};
+
+const drawUpperText = ({ ctx }) => {
+  ctx.font = `normal 700 ${fontSize}px 'Noto Sans TC', 'Noto Sans', sans-serif`;
+  const textWidth1 = ctx.measureText('GATHERING').width;
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.6)';
+  const text1X = hPadding;
+  const text1Y = vPadding + 70;
+  const text2X = text1X;
+  const text2Y = text1Y + 50;
+  ctx.fillRect(
+    text1X - 10,
+    text1Y - fontSize,
+    textWidth1 + 20,
+    (fontSize + 10) * 2
+  );
+
+  ctx.textAlign = 'left';
+  ctx.fillStyle = '#fafafa';
+  ctx.fillText('NODE', text1X, text1Y);
+  ctx.fillText('GATHERING', text2X, text2Y);
+};
+
+const drawLowerText = ({ ctx, name, date }) => {
+  ctx.font = `normal 700 ${fontSize -
+    10}px 'Noto Sans TC', 'Noto Sans', sans-serif`;
+  const textWidth2 = ctx.measureText(name).width;
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.6)';
+  const text3X = size - hPadding;
+  const text3Y = size - vPadding - 100;
+  const text4X = text3X;
+  const text4Y = text3Y + 60;
+  ctx.fillRect(
+    text3X - textWidth2 - 20,
+    text3Y - (fontSize - 0),
+    textWidth2 + 40,
+    (fontSize + 10) * 2 + 10
+  );
+
+  ctx.textAlign = 'right';
+  ctx.fillStyle = '#fafafa';
+  ctx.fillText(name, text3X, text3Y);
+  ctx.fillText(moment(date).format('MM/DD'), text4X, text4Y);
+};
+
 const previewLoad = ({
   canvasRef,
   rawRef,
@@ -33,10 +108,6 @@ const previewLoad = ({
   date,
   logoRef
 }) => {
-  const hPadding = 80;
-  const vPadding = 30;
-  const fontSize = 50;
-
   const canvas = canvasRef.current;
   const img = rawRef.current;
   // const logo = logoRef.current;
@@ -69,71 +140,11 @@ const previewLoad = ({
   //   size * logoRatio
   // );
 
-  ctx.lineWidth = 5;
-  ctx.strokeStyle = '#fafafa';
+  drawFrame({ ctx, size });
 
-  ctx.beginPath();
-  ctx.moveTo(hPadding, vPadding);
-  ctx.lineTo(size - hPadding, vPadding);
-  ctx.stroke();
-  ctx.closePath();
+  drawUpperText({ ctx });
 
-  ctx.beginPath();
-  ctx.moveTo(size - vPadding, hPadding);
-  ctx.lineTo(size - vPadding, size - hPadding);
-  ctx.stroke();
-  ctx.closePath();
-
-  ctx.beginPath();
-  ctx.moveTo(size - hPadding, size - vPadding);
-  ctx.lineTo(hPadding, size - vPadding);
-  ctx.stroke();
-  ctx.closePath();
-
-  ctx.beginPath();
-  ctx.moveTo(vPadding, size - hPadding);
-  ctx.lineTo(vPadding, hPadding);
-  ctx.stroke();
-  ctx.closePath();
-
-  ctx.font = `normal 700 ${fontSize}px 'Noto Sans TC', 'Noto Sans', sans-serif`;
-  const textWidth1 = ctx.measureText('GATHERING').width;
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.6)';
-  const text1X = hPadding;
-  const text1Y = vPadding + 70;
-  const text2X = text1X;
-  const text2Y = text1Y + 50;
-  ctx.fillRect(
-    text1X - 10,
-    text1Y - fontSize,
-    textWidth1 + 20,
-    (fontSize + 10) * 2
-  );
-
-  ctx.textAlign = 'left';
-  ctx.fillStyle = '#fafafa';
-  ctx.fillText('NODE', text1X, text1Y);
-  ctx.fillText('GATHERING', text2X, text2Y);
-
-  ctx.font = `normal 700 ${fontSize -
-    10}px 'Noto Sans TC', 'Noto Sans', sans-serif`;
-  const textWidth2 = ctx.measureText(name).width;
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.6)';
-  const text3X = size - hPadding;
-  const text3Y = size - vPadding - 100;
-  const text4X = text3X;
-  const text4Y = text3Y + 60;
-  ctx.fillRect(
-    text3X - textWidth2 - 20,
-    text3Y - (fontSize - 0),
-    textWidth2 + 40,
-    (fontSize + 10) * 2 + 10
-  );
-
-  ctx.textAlign = 'right';
-  ctx.fillStyle = '#fafafa';
-  ctx.fillText(name, text3X, text3Y);
-  ctx.fillText(moment(date).format('MM/DD'), text4X, text4Y);
+  drawLowerText({ ctx, name, date });
 
   const dataURL = canvas.toDataURL();
   setPreview(dataURL);
