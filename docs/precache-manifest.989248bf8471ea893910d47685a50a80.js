9861368017482194e0ee1782c06ff75c98863e43
JerryShen <jerry82422@gmail.com>
2024-09-04 09:16:42 +0800

    Build for demo

---
+++ docs/precache-manifest.989248bf8471ea893910d47685a50a80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e77ddd7a9d97441e74ad5cd645a47b18",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e137632bc57d268466e8",
+    "url": "/static/css/main.68a453bb.chunk.css"
+  },
+  {
+    "revision": "ca627de4b77ccd64195e",
+    "url": "/static/js/2.a7ff6f54.chunk.js"
+  },
+  {
+    "revision": "e137632bc57d268466e8",
+    "url": "/static/js/main.5e4cfd98.chunk.js"
+  },
+  {
+    "revision": "f060d5cc9783985b8daa",
+    "url": "/static/js/runtime-main.f93f920a.js"
+  },
+  {
+    "revision": "8c4f0b91204fee1a97d79391d666dc82",
+    "url": "/static/media/logo.8c4f0b91.png"
+  }
+]);
